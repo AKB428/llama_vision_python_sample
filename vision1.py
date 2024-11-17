b1ea971ff49eb7e85ffed2a80d35d9d3bae9aae0
AKB428 <gandara016+1@gmail.com>
2024-11-17 15:04:20 +0900

    fix prompt

--- vision1.py
+++ vision1.py
@@ -4,7 +4,7 @@ import sys
 inputImageFile = sys.argv[1]
 
 response = ollama.chat(
-    model='llama3.2-vision',
+    model='llama3.2-vision:11b',
     messages=[{
         'role': 'user',
         'content': 'What is in this image?',
--- vision2-tag2-jp.py
+++ vision2-tag2-jp.py
@@ -4,13 +4,16 @@ import sys
 inputImageFile = sys.argv[1]
 
 response = ollama.chat(
-    model='llama3.2-vision',
+    model='llama3.2-vision:11b',
     messages=[{
         'role': 'user',
         'content':  '''
+        あなたは画像を読み込んで内容のタグを出力する役割をもったプログラムです。
         なんの画像かを読み込んで、この画像を表すタグ名を5つ選んでJSON形式で書いてください。
-        例1 {tag: ["虎", "動物", "公園", "女性","アイス"] }
-        のような形で。その他の文言は返さないでください。
+        例1 {"tag": ["虎", "動物", "公園", "女性","アイス"] }
+        のような形で。
+        タグ名は日本語にしてください。
+        その他の文言は返さないでください。
         ''',
         'images': [inputImageFile]
     }]
